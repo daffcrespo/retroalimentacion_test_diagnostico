fdc139a4f81d5b55e88ab6471b3b474a81d04498
Dafne Crespo <dafne.crespo4000@alumnos.udg.mx>
2020-09-20 13:26:56 -0500

    Creamos un void para capturar el arreglo de 5 enteros

--- main.c
+++ main.c
@@ -1,17 +1,49 @@
 #include <stdio.h>
 
-int main(){
-    char op;
+
+void capturar_enteros(){
+    int enteros[5];
+    int i=0;
+    float promedio;
+    int suma=0;
+    printf("Digite los numeros: \n");
+    for(i=0; i<5; i++){
+        scanf("%i", &enteros[i]);
+        suma+=enteros[i];
+    }
+    for(i=0; i<5; i++){
+        printf("%i\n", enteros[i]);
+    }
+    promedio=suma/5;
+    printf("Suma: %i\n", suma);
+    printf("Promedio: %f\n", promedio);
+   
+}
+
+int main()
+{
+    int op;
     do{
         printf("1) Capturar enteros\n");
         printf("2) Mostrar cadena n veces\n");
         printf("3) Agregar personajes\n");
         printf("0) Salir\n");
-        scanf("%c", op);
-        
-        fflush(stdin);
+        printf("Que opcion quieres elegir?: ");
+        scanf("%i", &op);
+        switch (op)
+        {
+        case 1:
+            capturar_enteros();
+            break;
+        case 2:
 
-    }while (op!=0);
+            break;
+        case 3:
+            break;
+        default:
+            break;
+        }
+    }while (op != 0);
 
 }
-//{}   \
+//{}   \     []
